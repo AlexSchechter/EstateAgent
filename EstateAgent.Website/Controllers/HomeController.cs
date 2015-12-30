fa799164824ba0a486079f697e823a5be5fd436e
AlexSchechter <a.schechter@outlook.com>
2015-12-30 16:54:44 +0000

    Refactoring

--- EstateAgent.Website/Controllers/HomeController.cs
+++ EstateAgent.Website/Controllers/HomeController.cs
@@ -3,33 +3,35 @@ using Estates.Business;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using System.Linq;
+using System.Collections;
 
 namespace EstateAgent.Website.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        private EstateModel CreateEstateModel ()
+        {     
+                var model = new EstateModel();
+                model.Estates = EstateRepository.ListEstates().OrderBy(x => x.EstateID).ToList();
+                return model;
+        }
+
         public ActionResult Index()
         {
-            var model = new EstateModel();
-            model.Estates = EstateRepository.ListEstates().OrderBy(x => x.EstateID).ToList();
-            return View("MainPage", model);
+            return View("MainPage", CreateEstateModel());
         }
 
         public ActionResult Delete(int id)
         {
             EstateRepository.DeleteEstate(id);
-            var model = new EstateModel();
-            model.Estates = EstateRepository.ListEstates().OrderBy(x => x.EstateID).ToList();
-            return View("MainPage", model);
+            return View("MainPage", CreateEstateModel());
         }
 
         public ActionResult Add(string postcode, int bathrooms, int bedrooms, int squareFoot)
         {            
             EstateRepository.SaveEstate(bedrooms, bathrooms, postcode, squareFoot);
-            var model = new EstateModel();
-            model.Estates = EstateRepository.ListEstates().OrderBy(x => x.EstateID).ToList();
-            return View("MainPage",model);
+            return View("MainPage", CreateEstateModel());
         }
 
         public ActionResult Create()
