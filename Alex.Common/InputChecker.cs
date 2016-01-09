07f9ab92b1a29f27c1900a4b5743d2aa69a82d87
AlexSchechter <a.schechter@outlook.com>
2016-01-09 17:15:06 -0500

    Minor Refactoring

--- Alex.Common/InputChecker.cs
+++ Alex.Common/InputChecker.cs
@@ -4,6 +4,45 @@ namespace Alex.Common
 {
     public class InputChecker
     {
+        public static long AskCheckAndReAskForLongInput(string screenPromt)
+        {
+            string userInput = "";
+            long correctInput = 0;
+            while (!Int64.TryParse(userInput, out correctInput))
+            {
+                Console.WriteLine(screenPromt);
+                userInput = Console.ReadLine();
+            }
+
+            return correctInput;
+        }
+
+        public static long AskCheckAndReAskForLongInput(string screenPromt, int minValue)
+        {
+            string userInput = "";
+            long correctInput = 0;
+            while (!Int64.TryParse(userInput, out correctInput) || correctInput < minValue)
+            {
+                Console.WriteLine(screenPromt);
+                userInput = Console.ReadLine();
+            }
+
+            return correctInput;
+        }
+
+        public static long AskCheckAndReAskForLongInput(string screenPromt, int minValue, int maxValue)
+        {
+            string userInput = "";
+            long correctInput = 0;
+            while (!Int64.TryParse(userInput, out correctInput) || correctInput < minValue || correctInput > maxValue)
+            {
+                Console.WriteLine(screenPromt);
+                userInput = Console.ReadLine();
+            }
+
+            return correctInput;
+        }
+
         public static int AskCheckAndReAskForIntegerInput(string screenPromt)
         {
             string userInput = "";
